68dc6bbe265e92dab946216abf96e3df814406e0
Vu Hoang <hoangvv@icts.vn>
2021-03-03 20:13:36 +0700

    remove reactjs code

--- shoeshop.frontend/src/redux/actions/app-action.ts
+++
@@ -1,9 +0,0 @@
-import { createTypeAction } from '../type-redux';
-
-import * as Utils from '../../utilities/app-util';
-
-export const detectMobile = createTypeAction('DETECT_MOBILE', (userAgent: string) => {
-  return Utils.detectMobile(userAgent);
-});
-
-export const openCartDrawer = createTypeAction('OPEN_CART_DRAWER', (open: boolean) => open);
